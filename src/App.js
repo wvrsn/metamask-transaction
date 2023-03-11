b5073219d76b4ac15f058811906fcc2c091a2c48
Weverson Nascimento <weverson.sn+github@gmail.com>
2023-03-10 21:38:33 -0300

    Token transfer

--- src/App.js
+++ src/App.js
@@ -1,123 +1,103 @@
 import { useState } from "react";
-import { ethers } from "ethers";
 import ErrorMessage from "./ErrorMessage";
-import TxList from "./TxList";
-
-const chainIds = {
-  polygon: 137,
-  eth: 1,
-};
-
-const startPayment = async ({ chainId, setError, setTxs, ether, addr }) => {
-  try {
-    if (!window.ethereum)
-      throw new Error("No crypto wallet found. Please install it.");
-
-    await window.ethereum.send("eth_requestAccounts");
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    ethers.utils.getAddress(addr);
-
-    console.log(
-      window.ethereum.networkVersion,
-      chainId,
-      ethers.utils.hexlify(chainId)
-    );
-
-    if (window.ethereum.networkVersion !== chainId) {
-      try {
-        await window.ethereum.request({
-          method: "wallet_switchEthereumChain",
-          params: [{ chainId: ethers.utils.hexlify(chainId) }],
-        });
-      } catch (err) {
-        // This error code indicates that the chain has not been added to MetaMask
-        if (err.code === 4902) {
-          await window.ethereum.request({
-            method: "wallet_addEthereumChain",
-            params: [
-              {
-                chainName: "Polygon Mainnet",
-                chainId: ethers.utils.hexlify(chainId),
-                nativeCurrency: {
-                  name: "MATIC",
-                  decimals: 18,
-                  symbol: "MATIC",
-                },
-                rpcUrls: ["https://polygon-rpc.com/"],
-              },
-            ],
-          });
-        }
-      }
-    }
-
-    const tx = await signer.sendTransaction({
-      to: addr,
-      value: ethers.utils.parseEther(ether),
-    });
-
-    console.log({ ether, addr });
-    console.log("tx", tx);
-
-    setTxs([tx]);
-  } catch (err) {
-    setError(err.message);
-  }
-};
+import Web3 from "web3";
 
 export default function App() {
+  const [accounts, setAccounts] = useState([]);
   const [error, setError] = useState();
-  const [txs, setTxs] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const data = new FormData(e.target);
-    setError();
-    await startPayment({
-      chainId: chainIds.eth,
-      setError,
-      setTxs,
-      ether: data.get("ether"),
-      addr: data.get("addr"),
-    });
+  };
+
+  const connectWallet = async () => {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setAccounts(accounts);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  const sendTransaction = async () => {
+    try {
+      const web3 = new Web3();
+
+      const fromAddress = accounts[0];
+      const toAddress = "0x86CF57a8ABDc18049f21206a7cC0E5511BE6547A";
+      const amount = Number(1).toString(16);
+      const tokenAddress = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
+
+      const contract = new web3.eth.Contract(
+        [
+          {
+            constant: false,
+            inputs: [
+              {
+                name: "_to",
+                type: "address",
+              },
+              {
+                name: "_value",
+                type: "uint256",
+              },
+            ],
+            name: "transfer",
+            outputs: [
+              {
+                name: "",
+                type: "bool",
+              },
+            ],
+            payable: false,
+            stateMutability: "nonpayable",
+            type: "function",
+          },
+        ],
+        tokenAddress
+      );
+
+      const data = contract.methods.transfer(toAddress, amount).encodeABI();
+
+      const params = [
+        {
+          from: fromAddress,
+          to: tokenAddress,
+          data: data,
+        },
+      ];
+
+      await window.ethereum.request({
+        method: "eth_sendTransaction",
+        params,
+      });
+    } catch (err) {
+      setError(err.message);
+    }
   };
 
   return (
     <form className="m-4" onSubmit={handleSubmit}>
       <div className="credit-card w-full lg:w-1/2 sm:w-auto shadow-lg mx-auto rounded-xl bg-white">
-        <main className="mt-4 p-4">
-          <h1 className="text-xl font-semibold text-gray-700 text-center">
-            Send ETH payment
-          </h1>
-          <div className="">
-            <div className="my-3">
-              <input
-                type="text"
-                name="addr"
-                className="input input-bordered block w-full focus:ring focus:outline-none"
-                placeholder="Recipient Address"
-              />
-            </div>
-            <div className="my-3">
-              <input
-                name="ether"
-                type="text"
-                className="input input-bordered block w-full focus:ring focus:outline-none"
-                placeholder="Amount in ETH"
-              />
-            </div>
-          </div>
-        </main>
         <footer className="p-4">
           <button
-            type="submit"
-            className="btn btn-primary submit-button focus:ring focus:outline-none w-full"
+            type="button"
+            className="btn btn-primary submit-button focus:ring focus:outline-none w-full mb-2"
+            onClick={connectWallet}
           >
-            Pay now
+            Connect wallet
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary submit-button focus:ring focus:outline-none w-full"
+            onClick={sendTransaction}
+          >
+            Send Transaction
           </button>
           <ErrorMessage message={error} />
-          <TxList txs={txs} />
         </footer>
       </div>
     </form>
